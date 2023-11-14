041f276d70cf09b7bbafc9798e9c4691fe3564b2
jiawen9201 <waiwen@graduate.utm.my>
2023-11-14 18:13:53 +0800

    Revert "Update text_strings.dart"
    This reverts commit 830686f5110461a7780e2cc9c58f6ff8420beb05.

--- lib/src/constants/text_strings.dart
+++ lib/src/constants/text_strings.dart
@@ -45,7 +45,6 @@ const String otpMessage = "Enter the verification code sent at";
 const String profile = "Profile";
 const String editProfile = "Edit Profile";
 const String logout = "Logout";
-
 // Manage User Text
 const String manageUser = "Manage User";
 const String userList = "User List";
