e107b28b8a76cfd45296c4350fcdd439d4197402
jiawen9201 <waiwen@graduate.utm.my>
2023-12-02 17:29:17 +0800

    -

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\jiawa\OneDrive\Documents\GitHub\quickgrocer_application"
+export "FLUTTER_APPLICATION_PATH=C:\Users\wills\OneDrive - Universiti Teknologi Malaysia (UTM)\Documents\GitHub\quickgrocer_application"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
