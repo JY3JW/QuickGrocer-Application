67d5730592cb3a809c881d017b3f8dd465891b81
jiawen9201 <waiwen@graduate.utm.my>
2023-12-20 14:38:03 +0800

    Update make_feedback_screen.dart

--- lib/src/features/core/screens/feedback/make_feedback_screen.dart
+++ lib/src/features/core/screens/feedback/make_feedback_screen.dart
@@ -21,21 +21,19 @@ class _FeedbackScreenState extends State<FeedbackScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Scaffold(
-        appBar: AppBar(
-            leading: IconButton(
-                onPressed: () => Get.back(),
-                icon: Icon(LineAwesomeIcons.angle_left,
-                    color: iconColorWithoutBackground)),
-            title: Text(
-              feedback,
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            backgroundColor: Colors.transparent,
-            elevation: 0),
-        body: Feedback(order: widget.order),
-      ),
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+              onPressed: () => Get.back(),
+              icon: Icon(LineAwesomeIcons.angle_left,
+                  color: iconColorWithoutBackground)),
+          title: Text(
+            feedback,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0),
+      body: Feedback(order: widget.order),
     );
   }
 }
@@ -58,84 +56,86 @@ class _FeedbackState extends State<Feedback> {
     final _formKey = GlobalKey<FormState>();
     final controller = Get.put(FeedbackController());
 
-    return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    DropdownButtonFormField<String>(
-                      value: selectedValue,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please select a feedback category';
-                        }
-                        return null;
-                      },
-                      isExpanded: true,
-                      borderRadius: BorderRadius.circular(16),
-                      items: dropdownOptions.map((String value) {
-                        return DropdownMenuItem<String>(
-                            value: value, child: Text(value));
-                      }).toList(),
-                      onChanged: (value) async {
-                        setState(() {
-                          selectedValue = value!;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: formHeight - 20.0),
-                    TextFormField(
-                      controller: controller.description,
-                      maxLines: 8,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter the description of your feedback';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        label: Text('Description'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
+    return SingleChildScrollView(
+      child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child:
+                  Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      DropdownButtonFormField<String>(
+                        value: selectedValue,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please select a feedback category';
+                          }
+                          return null;
+                        },
+                        isExpanded: true,
+                        borderRadius: BorderRadius.circular(16),
+                        items: dropdownOptions.map((String value) {
+                          return DropdownMenuItem<String>(
+                              value: value, child: Text(value));
+                        }).toList(),
+                        onChanged: (value) async {
+                          setState(() {
+                            selectedValue = value!;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: formHeight - 20.0),
+                      TextFormField(
+                        controller: controller.description,
+                        maxLines: 8,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter the description of your feedback';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: Text('Description'),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: formHeight),
-                    SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            if (_formKey.currentState!.validate()) {
-                              await controller.createNewFeedback(FeedbackModel(
-                                  id: widget.order.id,
-                                  orderId: widget.order.id,
-                                  email: widget.order.email,
-                                  dateTime: DateTime.now(),
-                                  category: selectedValue == activeFeedback
-                                      ? feedbackTitle
-                                      : issueTitle,
-                                  description:
-                                      controller.description.text.trim()));
-                              controller.clearControllers();
-                              Navigator.pop(context);
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            shape: StadiumBorder(),
-                          ),
-                          child: const Text("Send Feedback"),
-                        )),
-                  ],
+                      const SizedBox(height: formHeight),
+                      SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {
+                                await controller.createNewFeedback(FeedbackModel(
+                                    id: widget.order.id,
+                                    orderId: widget.order.id,
+                                    email: widget.order.email,
+                                    dateTime: DateTime.now(),
+                                    category: selectedValue == activeFeedback
+                                        ? feedbackTitle
+                                        : issueTitle,
+                                    description:
+                                        controller.description.text.trim()));
+                                controller.clearControllers();
+                                Navigator.pop(context);
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: StadiumBorder(),
+                            ),
+                            child: const Text("Send Feedback"),
+                          )),
+                    ],
+                  ),
                 ),
-              ),
-            ])));
+              ]))),
+    );
   }
 }
