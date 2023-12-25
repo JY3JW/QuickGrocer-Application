b5a7c70df9e9c39ff6f163b5d272804b979dcf54
jiawen9201 <waiwen@graduate.utm.my>
2023-12-25 17:32:12 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,7 @@ Future<void> main() async {
     Get.put(UserRepository());
     Get.put(CartRepository());
   });
-  
-  WidgetsFlutterBinding.ensureInitialized();
+
   Stripe.publishableKey =
       "pk_live_51OOsGHH2s1vVpFNUw1bqdXg1tuCKZ3dYKG2iKnFW6LGazdEf6Uozt8uw6oj58hR81TFtwXGxdSh1wsivXV3NEqNK00XnFVBs2T";
   Stripe.instance.applySettings();
